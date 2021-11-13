4dd130b67a6209160e5cc98fe04d5339560cc037
spleekz <Shutnik-12K@yandex,ru>
2021-11-14 02:14:37 +0300

    change body styles to all styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { Header } from './components/Header'
 import { CardsPage } from './pages/Cards/CardsPage'
 
 const GlobalStyles = createGlobalStyle`
- body {
-  margin:0;
-  padding:0;
-  box-sizing:border-box;
-  background-color:#89a4ff;
-}
+  * {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+  body {
+    background-color:#89a4ff;
+  }
 `
 const AppContainer = styled.div``
 
