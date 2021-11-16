611f98149e2a2930f4c7f074ff036b6af1a62521
spleekz <Shutnik-12K@yandex,ru>
2021-11-17 01:49:42 +0300

    add browser router to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import { RootStoreProvider } from './stores/RootStore/RootStoreContext'
 
 ReactDOM.render(
-  <RootStoreProvider>
-    <App />
-  </RootStoreProvider>,
+  <BrowserRouter>
+    <RootStoreProvider>
+      <App />
+    </RootStoreProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
