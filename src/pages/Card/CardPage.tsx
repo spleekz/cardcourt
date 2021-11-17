7612d3e22dc01f3861bdcd3ccf1df768f6797cf4
spleekz <Shutnik-12K@yandex,ru>
2021-11-17 22:41:15 +0300

    create card page component

---
+++ src/pages/Card/CardPage.tsx
@@ -0,0 +1,24 @@
+import React, { FC, useEffect } from 'react'
+import styled from 'styled-components'
+import { useParams } from 'react-router'
+import { ICard } from '../../stores/CardsStore'
+import { useStore } from '../../stores/RootStore/RootStoreContext'
+import { observer } from 'mobx-react-lite'
+
+interface ICardProps {
+  card: ICard | null
+}
+
+const CardPageContainer = styled.div``
+
+export const Card: FC<ICardProps> = observer(({ card }): JSX.Element => {
+  const { CardsStore } = useStore()
+  const { cardId } = useParams()
+
+  useEffect(() => {
+    if (cardId) {
+      CardsStore.setCurrentCardId(cardId)
+    }
+  }, [cardId])
+  return <>{card && <CardPageContainer>{card.name}</CardPageContainer>}</>
+})
