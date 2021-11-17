e0fd830ea555fbaed9b9a17e707404f64404e893
spleekz <Shutnik-12K@yandex,ru>
2021-11-18 01:15:11 +0300

    add styles for cards page container

--- src/pages/Cards/CardsPage.tsx
+++ src/pages/Cards/CardsPage.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 import { useStore } from '../../stores/RootStore/RootStoreContext'
 import { CardList } from './CardList'
 
-const CardsPageContainer = styled.div``
+const CardsPageContainer = styled.div`
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
 
 export const CardsPage: FC = (): JSX.Element => {
   const { CardsStore } = useStore()
