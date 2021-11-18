af1be3f43d5f8fcbba564e155da8c0c6d5768718
spleekz <Shutnik-12K@yandex,ru>
2021-11-18 17:12:06 +0300

    add interface for WithSet

--- src/stores/entities/WithSet.ts
+++ src/stores/entities/WithSet.ts
@@ -1,6 +1,11 @@
 import { makeAutoObservable } from 'mobx'
 
-export class WithSet<T> {
+export interface IWithSet<T> {
+  value: T
+  set: (value: T) => void
+}
+
+export class WithSet<T> implements IWithSet<T> {
   value: T
   constructor(property: T) {
     makeAutoObservable(this)
