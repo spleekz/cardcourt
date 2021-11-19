f3f17bd9bae00f9da40890efa25b2dfc1eda2ab2
spleekz <Shutnik-12K@yandex,ru>
2021-11-19 22:17:23 +0300

    add WithBoolean entity

---
+++ src/stores/entities/WithBoolean.ts
@@ -0,0 +1,22 @@
+import { makeAutoObservable } from 'mobx'
+
+export interface IWithBoolean {
+  value: boolean
+  set: (value: boolean) => void
+  toggle: () => void
+}
+
+export class WithBoolean implements IWithBoolean {
+  value: boolean
+  constructor(value: boolean) {
+    makeAutoObservable(this)
+    this.value = value
+  }
+
+  set = (value: boolean): void => {
+    this.value = value
+  }
+  toggle = (): void => {
+    this.value = !this.value
+  }
+}
