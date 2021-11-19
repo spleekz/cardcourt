0d3b80121917ec6b88a12c2b6b4dee5e740e9874
spleekz <Shutnik-12K@yandex,ru>
2021-11-19 22:22:16 +0300

    add PlayCheck component

---
+++ src/pages/Check/PlayCheck/PlayCheck.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PlayCheckContainer = styled.div``
+
+export const PlayCheck: React.FC = (): JSX.Element => {
+  return <PlayCheckContainer>Play Check</PlayCheckContainer>
+}
