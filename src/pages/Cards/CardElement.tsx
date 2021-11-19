c3d445b8f73e2336cae5da143d1c92c7cc239f69
spleekz <Shutnik-12K@yandex,ru>
2021-11-19 22:19:32 +0300

    add CardElement component

---
+++ src/pages/Cards/CardElement.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { ICard } from '../../stores/CardsStore'
+import { observer } from 'mobx-react-lite'
+
+interface ICardProps {
+  card: ICard
+}
+
+const CardContainer = styled.div<{ color: string }>`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 320px;
+  height: 500px;
+  margin: 0 8px 0 8px;
+  background-color: ${(props) => props.color};
+  border-radius: 16px;
+  overflow: hidden;
+`
+const CardHeading = styled.div<{ color: string }>`
+  padding: 2px 15px;
+  background-color: ${(props) => props.color};
+`
+const CardTitle = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+`
+const CardAuthor = styled.div`
+  font-size: 25px;
+  color: #000000a0;
+`
+const CardWords = styled.div<{ color: string; positionTop: number }>`
+  min-height: 502px;
+  position: relative;
+  top: ${(props) => props.positionTop + 'px'};
+  left: 0;
+  margin-top: 10px;
+  background-color: ${(props) => props.color};
+  padding: 4px 15px;
+  border-radius: 16px 16px 0 0;
+  transition: 0.4s;
+`
+const CardWordContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`
+const CardWord = styled.span``
+const Dash = styled.span``
+const PlaceForCursor = styled.div`
+  position: absolute;
+  top: 100px;
+  left: 15px;
+  z-index: 1000;
+  width: 90%;
+  height: 215px;
+`
+
+export const CardElement: React.FC<ICardProps> = observer(({ card }): JSX.Element => {
+  const [cardWordsPosition, setCardWordsPosition] = useState<number>(0)
+
+  return (
+    <Link key={card.id} to={`/card/${card.id}`}>
+      <CardContainer key={card.id} color={card.ui.headColor}>
+        <PlaceForCursor
+          onMouseEnter={() => setCardWordsPosition(-90)}
+          onMouseLeave={() => setCardWordsPosition(0)}
+        />
+        <CardHeading color={card.ui.headColor}>
+          <CardTitle>{card.name}</CardTitle>
+          <CardAuthor>{card.author}</CardAuthor>
+        </CardHeading>
+        <CardWords color={card.ui.wordListColor} positionTop={cardWordsPosition}>
+          {card.wordList.map((word) => {
+            return (
+              <CardWordContainer key={word.id}>
+                <CardWord>{word.en}</CardWord>
+                <Dash>—</Dash>
+                <CardWord>{word.ru}</CardWord>
+              </CardWordContainer>
+            )
+          })}
+        </CardWords>
+      </CardContainer>
+    </Link>
+  )
+})
--- src/pages/Cards/CardList.tsx
+++ src/pages/Cards/CardList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import { ICard } from '../../stores/CardsStore'
+import { CardElement } from './CardElement'
 
 interface ICardListProps {
   cards: Array<ICard>
@@ -10,74 +10,12 @@ interface ICardListProps {
 const CardListContainer = styled.div`
   display: flex;
 `
-const CardContainer = styled.div<{ color: string }>`
-  display: flex;
-  flex-direction: column;
-  width: 320px;
-  height: 500px;
-  margin: 0 8px 0 8px;
-  background-color: ${(props) => props.color};
-  border-radius: 16px;
-  overflow: hidden;
-`
-const CardHeading = styled.div<{ color: string }>`
-  padding: 2px 15px;
-  background-color: ${(props) => props.color};
-`
-const CardTitle = styled.div`
-  font-size: 40px;
-  font-weight: bold;
-`
-const CardAuthor = styled.div`
-  font-size: 25px;
-  color: #000000a0;
-`
-const CardWords = styled.div<{ color: string }>`
-  min-height: 502px;
-  position: relative;
-  top: 0;
-  left: 0;
-  margin-top: 10px;
-  background-color: ${(props) => props.color};
-  padding: 4px 15px;
-  border-radius: 16px 16px 0 0;
-  transition: 0.4s;
-  &:hover {
-    top: -90px;
-  }
-`
-const CardWordContainer = styled.div`
-  display: flex;
-  font-size: 30px;
-`
-const CardWord = styled.span``
-const Dash = styled.span``
 
 export const CardList: React.FC<ICardListProps> = ({ cards }) => {
   return (
     <CardListContainer>
       {cards.map((card) => {
-        return (
-          <Link key={card.id} to={`/card/${card.id}`}>
-            <CardContainer key={card.id} color={card.ui.headColor}>
-              <CardHeading color={card.ui.headColor}>
-                <CardTitle>{card.name}</CardTitle>
-                <CardAuthor>{card.author}</CardAuthor>
-              </CardHeading>
-              <CardWords color={card.ui.wordListColor}>
-                {card.wordList.map((word) => {
-                  return (
-                    <CardWordContainer key={word.id}>
-                      <CardWord>{word.en}</CardWord>
-                      <Dash>—</Dash>
-                      <CardWord>{word.ru}</CardWord>
-                    </CardWordContainer>
-                  )
-                })}
-              </CardWords>
-            </CardContainer>
-          </Link>
-        )
+        return <CardElement card={card} key={card.id} />
       })}
     </CardListContainer>
   )
