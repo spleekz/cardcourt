6e9278a5a51eebbeeae7dac5fbe669607ca531eb
spleekz <Shutnik-12K@yandex,ru>
2021-11-19 22:18:36 +0300

    add createCheckStore function

--- src/stores/RootStore/RootStore.ts
+++ src/stores/RootStore/RootStore.ts
@@ -1,8 +1,14 @@
 import { CardsStore, ICardsStore } from '../CardsStore'
+import { ICheckStore, CheckStore } from '../CheckStore'
+
 export interface IRootStore {
   CardsStore: ICardsStore
+  createCheckStore: () => ICheckStore
 }
 
 export class RootStore implements IRootStore {
   CardsStore = new CardsStore()
+  createCheckStore = (): ICheckStore => {
+    return new CheckStore()
+  }
 }
