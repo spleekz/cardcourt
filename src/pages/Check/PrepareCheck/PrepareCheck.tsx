92ea31379027d2a34487b611c0fe84a75cb0d999
spleekz <Shutnik-12K@yandex,ru>
2021-11-19 22:22:26 +0300

    add PrepareCheck component

---
+++ src/pages/Check/PrepareCheck/PrepareCheck.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PrepareCheckContainer = styled.div``
+
+export const PrepareCheck: React.FC = (): JSX.Element => {
+  return <PrepareCheckContainer>Prepare Check</PrepareCheckContainer>
+}
