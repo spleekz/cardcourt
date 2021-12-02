66d333071e14088944e8c971f640024176e7850f
spleekz <Shutnik-12K@yandex,ru>
2021-12-02 20:36:22 +0300

    add wordList component

---
+++ src/components/WordList.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ICard } from '../stores/CardsStore'
+
+const CardWordContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 6px;
+`
+const CardWordBlock = styled.span`
+  font-size: 32px;
+  font-weight: bold;
+  border-radius: 12px;
+  padding: 6px;
+  background: #ffffff;
+`
+const CardWord = styled.span``
+const Dash = styled.span``
+
+export const WordList: React.FC<{ card: ICard }> = ({ card }) => {
+  return (
+    <>
+      {card.wordList.map((word) => {
+        return (
+          <CardWordContainer key={word.id}>
+            <CardWordBlock>
+              <CardWord>{word.en}</CardWord>
+              <Dash>—</Dash>
+              <CardWord>{word.ru}</CardWord>
+            </CardWordBlock>
+          </CardWordContainer>
+        )
+      })}
+    </>
+  )
+}
