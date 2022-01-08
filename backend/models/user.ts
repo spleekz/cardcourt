265b1e7c4e5030e4a6229b3efb9315c43e131370
spleekz <Shutnik-12K@yandex,ru>
2022-01-08 19:50:35 +0300

    rename repository

--- backend/models/user.ts
+++
@@ -1,8 +0,0 @@
-import { createSchema, Type, typedModel } from 'ts-mongoose'
-
-const UserSchema = createSchema({
-  name: Type.string({ required: true }),
-  password: Type.string({ required: true }),
-})
-
-export const User = typedModel('User',UserSchema)
