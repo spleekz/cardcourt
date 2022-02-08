f8af71d02cd432142fb02e8b537856302bfc9175
spleekz <Shutnik-12K@yandex,ru>
2022-02-08 11:21:06 +0300

    move slider buttons to slider component

--- src/pages/card-court/court/card-slider.tsx
+++ src/pages/card-court/court/card-slider.tsx
@@ -1,27 +1,59 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { CardRef } from '../../../components/cards/card-ref'
 import { Card } from '../../../api/api'
+import { useStore } from '../../../stores/root-store/context'
 
 interface CardSliderProps {
   cards: Array<Card>
-  position: number
 }
 
-export const CardSlider: React.FC<CardSliderProps> = observer(({ cards, position }) => {
+export const CardSlider: React.FC<CardSliderProps> = observer(({ cards }) => {
+  const { cardsStore } = useStore()
+
+  const [position, setPosition] = useState<number>(0)
+
+  const prevPage = (): void => {
+    cardsStore.setPrevPage()
+    setPosition((current) => current - 1682)
+  }
+  const nextPage = (): void => {
+    cardsStore.setNextPage()
+    if (!cardsStore.pageWasVisited && cardsStore.notLastPage) {
+      cardsStore.loadMoreCards()
+    }
+    setPosition((current) => current + 1682)
+  }
+
   return (
     <SliderContainer>
-      <SliderLine position={position}>
-        {cards.map((card) => {
-          return <CardRef type='element' card={card} key={card._id} />
-        })}
-      </SliderLine>
+      {cardsStore.pageCount > 1 && (
+        <SliderButton onClick={prevPage} disabled={cardsStore.page <= 1}>
+          Назад
+        </SliderButton>
+      )}
+      <SliderWindow>
+        <SliderLine position={position}>
+          {cards.map((card) => {
+            return <CardRef type='element' card={card} key={card._id} />
+          })}
+        </SliderLine>
+      </SliderWindow>
+      {cardsStore.pageCount > 1 && (
+        <SliderButton onClick={nextPage} disabled={cardsStore.page === cardsStore.pageCount}>
+          Вперёд
+        </SliderButton>
+      )}
     </SliderContainer>
   )
 })
 
 const SliderContainer = styled.div`
+  display: flex;
+`
+
+const SliderWindow = styled.div`
   position: relative;
   width: 1682px;
   height: 500px;
@@ -33,3 +65,4 @@ const SliderLine = styled.div<{ position: number }>`
   transform: ${(props) => `translateX(-${props.position}px)`};
   transition: 0.48s;
 `
+const SliderButton = styled.button``
--- src/pages/card-court/court/court.tsx
+++ src/pages/card-court/court/court.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
@@ -8,40 +8,16 @@ import { useStore } from '../../../stores/root-store/context'
 export const Court: React.FC = observer(() => {
   const { cardsStore } = useStore()
 
-  const [position, setPosition] = useState<number>(0)
-
-  const prevPage = (): void => {
-    cardsStore.setPrevPage()
-    setPosition((current) => current - 1682)
-  }
-  const nextPage = (): void => {
-    cardsStore.setNextPage()
-    if (!cardsStore.pageWasVisited && cardsStore.notLastPage) {
-      cardsStore.loadMoreCards()
-    }
-    setPosition((current) => current + 1682)
-  }
-
   useEffect(() => {
     cardsStore.loadCards(1, 2)
   }, [cardsStore.search])
 
   return (
     <CardListContainer>
-      {cardsStore.pageCount > 1 && (
-        <SliderButton onClick={prevPage} disabled={cardsStore.page <= 1}>
-          Назад
-        </SliderButton>
-      )}
       <Link to='/card/new'>
         <CreateCardButton>Создать</CreateCardButton>
       </Link>
-      <CardSlider cards={cardsStore.cards} position={position} />
-      {cardsStore.pageCount > 1 && (
-        <SliderButton onClick={nextPage} disabled={cardsStore.page === cardsStore.pageCount}>
-          Вперёд
-        </SliderButton>
-      )}
+      <CardSlider cards={cardsStore.cards} />
     </CardListContainer>
   )
 })
@@ -56,4 +32,3 @@ const CreateCardButton = styled.button`
   top: -30px;
   margin: 0px 0px 16px 8px;
 `
-const SliderButton = styled.button``
