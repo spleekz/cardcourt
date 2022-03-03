eca40b4e6d8f607238588a0574356f18b261eae4
spleekz <Shutnik-12K@yandex.ru>
2022-03-04 01:55:35 +0300

    add functions for getting card width and height

---
+++ src/lib/cards.ts
@@ -0,0 +1,11 @@
+import cardConfig from '../stores/card-config.json'
+
+const widthToHeight = cardConfig.width / cardConfig.height
+const heightToWidth = cardConfig.height / cardConfig.width
+
+export const getCardWidthByHeight = (height: number): number => {
+  return height * widthToHeight
+}
+export const getCardHeightByWidth = (width: number): number => {
+  return width * heightToWidth
+}
