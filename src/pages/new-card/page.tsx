8247c6a4e915fef2276ef04343ed1aae5f4e075c
spleekz <Shutnik-12K@yandex.ru>
2022-04-19 23:07:34 +0300

    add preloader to new card page if loading me

--- src/pages/new-card/page.tsx
+++ src/pages/new-card/page.tsx
@@ -2,13 +2,21 @@ import { observer } from 'mobx-react-lite'
 import React from 'react'
 import styled from 'styled-components'
 import { FormCard } from '../../components/card/variants/form-card/form-card'
+import { ScreenPreloader } from '../../components/icons/screen-preloader'
 import { registerPage } from '../../hocs/register-page'
+import { useStore } from '../../stores/root-store/context'
 import { getCardWidthByHeight } from '../../utils/cards'
 
 export const NewCardPage: React.FC = registerPage(
   observer(() => {
+    const { authStore } = useStore()
+
     const cardHeight = 780
     const cardWidth = getCardWidthByHeight(cardHeight)
+
+    if (authStore.isLoadingMe) {
+      return <ScreenPreloader />
+    }
 
     return (
       <Container>
