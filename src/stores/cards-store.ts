4fc824e63c4c387338439a0f460e4ba6ab021198
spleekz <Shutnik-12K@yandex.ru>
2022-05-02 23:53:07 +0300

    remove actions to update cards from cards store

--- src/stores/cards-store.ts
+++ src/stores/cards-store.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable } from 'mobx'
 import { Cards, SendedCard, UpdatedCard } from '../api/api'
 import { createCard, updateCard } from '../api'
-import { ActionToUpdateCards } from './stores-utility-types'
 import { CreateCardResponsePromise, UpdateCardResponsePromise } from '../api/api-utility-types'
 
 export class CardsStore {
@@ -10,14 +9,6 @@ export class CardsStore {
   }
 
   cards: Cards = []
-
-  setCards: ActionToUpdateCards = (cards) => {
-    this.cards.length = 0
-    this.cards.push(...cards)
-  }
-  pushCards: ActionToUpdateCards = (cards) => {
-    this.cards.push(...cards)
-  }
 
   createCard(card: SendedCard): CreateCardResponsePromise {
     return createCard(card)
