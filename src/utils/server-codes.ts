58b5fa86411a199a8562426d63d374e406557791
spleekz <Shutnik-12K@yandex.ru>
2022-05-18 00:12:43 +0300

    fix isSuccess and isError code fns

--- src/utils/server-codes.ts
+++ src/utils/server-codes.ts
@@ -1,8 +1,8 @@
 export const isSuccessCode = (code: number): boolean => {
-  return code % 100 === 2
+  return code / 100 === 2
 }
 export const isErrorCode = (code: number): boolean => {
-  return code % 100 === 4 || code % 100 === 5
+  return code / 100 === 4 || code / 100 === 5
 }
 
 export const isUnknownError = (errorCode: number, handledErrors: Array<number>): boolean => {
