34cceeef46efb5cbacf423f607adf3b80c773397
spleekz <Shutnik-12K@yandex.ru>
2022-06-29 18:16:20 +0300

    useClickOutside can take ref or array of refs

--- src/hooks/use-click-outside.tsx
+++ src/hooks/use-click-outside.tsx
@@ -3,19 +3,35 @@ import React, { useEffect } from 'react'
 import { EmptyFunction } from 'basic-utility-types'
 
 type Config = {
-  ref: React.RefObject<HTMLElement | null>
+  ref: React.RefObject<HTMLElement | null> | Array<React.RefObject<HTMLElement | null>>
   fn: EmptyFunction
 }
 
 export const useClickOutside = ({ ref, fn }: Config): void => {
   useEffect(() => {
+    const checkIfClickOutsideElement = (element: HTMLElement | null, event: Event): boolean => {
+      return !element?.contains(event.target as Node)
+    }
+
     const handleClickOutside = (event: Event): void => {
-      if (!ref.current?.contains(event.target as Node)) {
+      let isClickOutside = false
+      if (Array.isArray(ref)) {
+        isClickOutside = ref.every((refElement) =>
+          checkIfClickOutsideElement(refElement.current, event),
+        )
+      } else {
+        isClickOutside = checkIfClickOutsideElement(ref.current, event)
+      }
+
+      if (isClickOutside) {
         fn()
       }
     }
 
-    if (ref.current) {
+    if (
+      (Array.isArray(ref) && ref.every((refElement) => refElement.current !== null)) ||
+      (!Array.isArray(ref) && ref.current !== null)
+    ) {
       document.addEventListener('click', handleClickOutside, true)
     }
 
