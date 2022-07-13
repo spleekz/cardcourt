3e0529ec432bf2048302c0a5a37c6d0f330e452e
spleekz <Shutnik-12K@yandex.ru>
2022-07-13 22:13:24 +0300

    go next after skip word

--- src/stores/card-check-store/play-session/play-session-store.ts
+++ src/stores/card-check-store/play-session/play-session-store.ts
@@ -117,7 +117,7 @@ export class CardCheckPlaySessionStore {
 
   skipCurrentWord(): void {
     this.updateResultWords('skipped')
-    this.goToNextWord()
+    this.goNext()
   }
 
   checkUserTranslate(): boolean | undefined {
