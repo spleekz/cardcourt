507ca8ce4b452c2cbd3d8f4dc26633be81f40a27
spleekz <Shutnik-12K@yandex.ru>
2022-07-15 02:33:16 +0300

    add options for useClickOutside

--- src/hooks/use-click-outside.tsx
+++ src/hooks/use-click-outside.tsx
@@ -2,12 +2,19 @@ import React, { useEffect } from 'react'
 
 import { EmptyFunction } from 'basic-utility-types'
 
+type UseClickOutsideOptions = {
+  eventType?: 'click' | 'mousedown' | 'mouseup'
+}
+
 type Config = {
   ref: React.RefObject<HTMLElement | null> | Array<React.RefObject<HTMLElement | null>>
   fn: EmptyFunction
+  options?: UseClickOutsideOptions
 }
 
-export const useClickOutside = ({ ref, fn }: Config): void => {
+export const useClickOutside = ({ ref, fn, options }: Config): void => {
+  const { eventType = 'click' } = options ?? {}
+
   useEffect(() => {
     const checkIfClickOutsideElement = (element: HTMLElement | null, event: Event): boolean => {
       return !element?.contains(event.target as Node)
@@ -32,9 +39,9 @@ export const useClickOutside = ({ ref, fn }: Config): void => {
       (Array.isArray(ref) && ref.every((refElement) => refElement.current !== null)) ||
       (!Array.isArray(ref) && ref.current !== null)
     ) {
-      document.addEventListener('click', handleClickOutside, true)
+      document.addEventListener(eventType, handleClickOutside, true)
     }
 
-    return () => document.removeEventListener('click', handleClickOutside, true)
-  }, [fn])
+    return () => document.removeEventListener(eventType, handleClickOutside, true)
+  })
 }
